24517f2d46099a505da6ca304924d11b00397785
NEU-SiyuanXu <xu.siyu@northeastern.edu>
2022-10-11 09:10:26 -0400

    assignment3 completed

--- assignment-movie-database/src/main/scala/edu/neu/coe/csye7200/asstmd/Movie.scala
+++ assignment-movie-database/src/main/scala/edu/neu/coe/csye7200/asstmd/Movie.scala
@@ -98,7 +98,7 @@ object Movie extends App {
       * @param w a line of input.
       * @return a Try[Movie]
       */
-    def parse(w: String): Try[Movie] = ??? // TO BE IMPLEMENTED
+    def parse(w: String): Try[Movie] = Try(Movie(w.split(",").toSeq))
   }
 
   val ingester = new Ingest[Movie]()
@@ -119,9 +119,7 @@ object Movie extends App {
   def elements(list: Seq[String], indices: Int*): List[String] = {
     // Hint: form a new list which is consisted by the elements in list in position indices. Int* means array of Int.
     // 6 points
-    val result: Seq[String] =
-    // TO BE IMPLEMENTED
-    ???
+    val result: Seq[String] = for(i <- indices) yield list(i)
     result.toList
   }
 
@@ -201,7 +199,10 @@ object Rating {
     */
   // Hint: This should similar to apply method in Object Name. The parameter of apply in case match should be same as case class Rating
   // 13 points
-  def apply(s: String): Rating = ??? // TO BE IMPLEMENTED
+  def apply(s: String): Rating = s match {
+    case rRating(code, _, age) => apply(code,Try(age.toInt).toOption)
+    case _ => throw ParseException(s"parse error in Rating: $Rating (parsed as $s)")
+  }
 
 }
 
